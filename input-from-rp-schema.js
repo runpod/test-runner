4f6c3f73ba94b4afb7b1bdb0ecb72a60cbce06a3
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2023-07-10 10:43:30 -0400

    get rid of some unused files

--- input-from-rp-schema.js
+++
@@ -1,5 +0,0 @@
-// in: file path for rp schema, possible values of required inputs
-// out: representative set of inputs for each parameter
-// for parameter in schema:
-//     try not specifying parameter
-//     try specifying only parameter (and required inputs)
