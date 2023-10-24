80c3d7b48f26027c8a1581cc438c6008d3a9d37c
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2023-10-24 13:48:53 -0400

    rm unused files

--- print-all-combos.js
+++
@@ -1,9 +0,0 @@
-import { xprod } from "ramda"
-import hardwareConfigs from "./hardwareConfigs.json" assert { type: "json" }
-import inputs from "./inputs.json" assert { type: "json" }
-
-const combos = xprod(hardwareConfigs, inputs).map(([hardwareConfig, input]) => ({
-  hardwareConfig,
-  input,
-}))
-console.log(JSON.stringify(combos, null, 2))
